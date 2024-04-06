6b73263800279ddb96b3429dea2366b03b9e4036
Wesley Santos <84673115+iW3ll@users.noreply.github.com>
2024-04-05 23:46:28 -0300

    Update vkUploader.py

--- vkUploader.py
+++ vkUploader.py
@@ -1,12 +1,12 @@
 import vk_api
 from vk_api import VkUpload
 
-# Your VK.com login and password
+# Your VK login and password
 LOGIN = ''
 PASSWORD = ''
 
 # Read video file paths from a text file
-video_file_path = r'' #put path to file here
+video_file_path = r'' # Put path to file here
 with open(video_file_path, 'r') as file:
     video_files = file.readlines()
 video_files = [video_file.strip() for video_file in video_files]
@@ -20,7 +20,7 @@ upload = VkUpload(vk)
 # Upload videos
 for video_file in video_files:
     # Upload video
-    video = upload.video(video_file=video_file, is_private=1) #if 0 = public
+    video = upload.video(video_file=video_file, is_private=1) # If 0 = public
     
     # Print URL of the uploaded video
     print('Video URL:', video['video']['player'])
